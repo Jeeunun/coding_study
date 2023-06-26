cefaf6490322620804773b8fd143a44155f53b85
Jeeunun <hanjieun7290@gmail.com>
2023-06-26 13:14:29 +0900

    구현

--- 20230620.py
+++ 20230620.py
@@ -1,94 +1,110 @@
-# # <만들 수 없는 금액>
-# # 만들 수 있는 금액(tot) 을 생각해보자.
-# # tot과 비교할 수 있는 값 x 설정
-# # coins 정렬 후 차례대로 동전을 추가해가며 만들 수 없는 금액을 찾아보자 !
+# # # <만들 수 없는 금액>
+# # # 만들 수 있는 금액(tot) 을 생각해보자.
+# # # tot과 비교할 수 있는 값 x 설정
+# # # coins 정렬 후 차례대로 동전을 추가해가며 만들 수 없는 금액을 찾아보자 !
 
-# # 1. 예제
-# coins = [3,2,1,1,9]
-# coins.sort() #[1,1,2,3,9]
+# # # 1. 예제
+# # coins = [3,2,1,1,9]
+# # coins.sort() #[1,1,2,3,9]
 
-#     #1) tot = 1 
-#     #
-#     #2) 1원을 추가했을 때 
-#     #  tot = 2 
-#     #  x = 2 + 1 = 3
-#     #
-#     #3) 2원을 추가했을 때  
-#     #  tot = 4
-#     #  x=  3 + 2 = 5
-#     #   
-#     #4) 3원을 추가 했을 때
-#     #  tot = 7
-#     #  x = 5 + 3 = 8
-#     #
-#     #5) 9원을 추가 했을 때  
-#     # x보다 크다. => 만들 수 없는 최솟값은 8 !
+# #     #1) tot = 1 
+# #     #
+# #     #2) 1원을 추가했을 때 
+# #     #  tot = 2 
+# #     #  x = 2 + 1 = 3
+# #     #
+# #     #3) 2원을 추가했을 때  
+# #     #  tot = 4
+# #     #  x=  3 + 2 = 5
+# #     #   
+# #     #4) 3원을 추가 했을 때
+# #     #  tot = 7
+# #     #  x = 5 + 3 = 8
+# #     #
+# #     #5) 9원을 추가 했을 때  
+# #     # x보다 크다. => 만들 수 없는 최솟값은 8 !
 
-# # 2. 프로그램 구현
-# coins = list(map(int, input("가지고 있는 화폐 단위를 입력하세요: ")))
-# coins.sort() 
+# # # 2. 프로그램 구현
+# # coins = list(map(int, input("가지고 있는 화폐 단위를 입력하세요: ")))
+# # coins.sort() 
 
-# x = 1 # 만들 수 없는 금액과의 비교를 위해 만들 수 없는 금액 가상 설정
-# tot = 0 # 만들 수 있는 금액의 범위(누적합)
+# # x = 1 # 만들 수 없는 금액과의 비교를 위해 만들 수 없는 금액 가상 설정
+# # tot = 0 # 만들 수 있는 금액의 범위(누적합)
 
 
-# for coin in coins :
-#     tot+=coin
-#     if coin > x :
-#         break
-#     x += coin
-# print(x)
-#     # coins = [1,1,2,3,9] 일 때 
-#     # coin 1 -> x = 1, tot = 1 -> x = 2 #=최솟값 1을 만들 수 있다. 
-#     # coin 1 -> tot = 2, x = 2 -> x = 3 #=최솟값 2을 만들 수 있다.
-#     # coin 2 -> tot = 4, x = 3 -> x = 5 #=최솟값 3을 만들 수 있다.
-#     # coin 3 -> tot = 7, x = 5 -> x = 8 #=최솟값 5를 만들 수 있다.
-#     # coin 9 -> tot =16, x = 8 -> break #=최솟값 8 !
+# # for coin in coins :
+# #     tot+=coin
+# #     if coin > x :
+# #         break
+# #     x += coin
+# # print(x)
+# #     # coins = [1,1,2,3,9] 일 때 
+# #     # coin 1 -> x = 1, tot = 1 -> x = 2 #=최솟값 1을 만들 수 있다. 
+# #     # coin 1 -> tot = 2, x = 2 -> x = 3 #=최솟값 2을 만들 수 있다.
+# #     # coin 2 -> tot = 4, x = 3 -> x = 5 #=최솟값 3을 만들 수 있다.
+# #     # coin 3 -> tot = 7, x = 5 -> x = 8 #=최솟값 5를 만들 수 있다.
+# #     # coin 9 -> tot =16, x = 8 -> break #=최솟값 8 !
 
-# # 3. 함수
-# def solution(coins):   
-#     coins.sort() 
-#     x = 1
-#     tot = 0
-#     for coin in coins :
-#         tot+=coin
-#         if coin > x :
-#             break
-#         x += coin
-#     return x
+# # # 3. 함수
+# # def solution(coins):   
+# #     coins.sort() 
+# #     x = 1
+# #     tot = 0
+# #     for coin in coins :
+# #         tot+=coin
+# #         if coin > x :
+# #             break
+# #         x += coin
+# #     return x
 
-# coins = list(map(int, input("가지고 있는 화폐 단위를 입력하세요: ").split(' ')))
-# result = solution(coins)
-# print(result)
+# # coins = list(map(int, input("가지고 있는 화폐 단위를 입력하세요: ").split(' ')))
+# # result = solution(coins)
+# # print(result)
 
-def solution(x : list,n: int):
-    x.sort()
-    if x[0]!=1:
-        return 1
-    else:
-        #dictionary 완성
-        use_dict=dict.fromkeys(x,0)
-        for number in use_dict:
-            use_dict[number]=list(map(lambda i:number*i,range(x.count(number)+1)))
-        print(use_dict)
-        result=[0]
-        for plus_num in use_dict:
-            result=list(set(sum([list(map(lambda x:x+y,use_dict[plus_num])) for y in result],[])))
-            print(result)
+# def solution(x : list,n: int):
+#     x.sort()
+#     if x[0]!=1:
+#         return 1
+#     else:
+#         #dictionary 완성
+#         use_dict=dict.fromkeys(x,0)
+#         for number in use_dict:
+#             use_dict[number]=list(map(lambda i:number*i,range(x.count(number)+1)))
+#         print(use_dict)
+#         result=[0]
+#         for plus_num in use_dict:
+#             result=list(set(sum([list(map(lambda x:x+y,use_dict[plus_num])) for y in result],[])))
+#             print(result)
         
-        return result
+#         return result
     
-result= solution([1,1,2,2,3],2) 
-n = 3
-print(result)
-        # #아랜 정답 채크용
-        # answer=sum(x)+1
-        # for final in range(1,answer+1):
-        #     if final not in result:
-        #         answer=final
-        #         break
-        # return answer
+# result= solution([1,1,2,2,3],2) 
+# n = 3
+# print(result)
+#         # #아랜 정답 채크용
+#         # answer=sum(x)+1
+#         # for final in range(1,answer+1):
+#         #     if final not in result:
+#         #         answer=final
+#         #         break
+#         # return answer
 
+def money(prices):
+    prices.sort()
+    minimum = 1 # 만들 수 없는 최솟값의 초기 값
+    for a in prices:
+        
+        if a <= minimum:
+            # print(minimum)
+            minimum += a
+            
+
+    return minimum
+prices1 = [3, 2, 1, 1, 9]
+print(money(prices1))
+
+prices2 = [1, 3, 5, 7]
+print(money(prices2))
 
 # # # 궁금한 점 : N은 언제 써야 하는지..?
 
---
+++ 20230627.py
@@ -0,0 +1,101 @@
+# # p110 예제 4-1 <상하좌우>
+# # <아이디어>
+# # N X N 정사각형 -> 가장 왼쪽 위 좌표(1,1) / 가장 오른쪽 아래 좌표(N,N)
+# # 위치 좌표 (X,Y)라 할 때
+# # L(왼쪽이동) : Y 의 변화(-)
+# # R(오른쪽이동) : Y 의 변화 (+)
+# # U (위로 이동) : X 의 변화 (-) => X=1일때 위로 이동시, 공간범위 벗어남(조건문으로 해결)
+# # D (아래로 이동) : X의 변화 (+) => X=N일때 아래로 이동시, 공간범위 벗어남(조건문으로 해결)
+
+# # <예제>
+# now = [1,1] #현재위치
+# N = int(input("공간의 크기를 설정하세요: "))
+# plan = list(input("어느 방향으로 갈까요?(L,R,U,D): "))
+
+# for p in plan:
+#     if p == 'L':
+#         now = (now[0], now[1] - 1)
+#         if now[1] < 1 :
+#             now = (now[0],now[1]+1)
+#     elif p == 'R':
+#         now = (now[0], now[1] + 1) 
+#         if now[1] < 1 :
+#             now = (now[0],now[1]-1)   
+#     elif p == 'U':
+#         now = (now[0] - 1,now[1])
+#         if now[0] < 1:
+#             now = (now[0]+1,now[1])
+#     elif p == 'D':
+#         now = (now[0] + 1, now[1])
+#         if now[0] > N:
+#             now = (now[0]-1, now[1])
+
+# print(now)
+
+# # <프로그램구현>
+# def solution(N,plan):
+#     now = [1,1]
+#     for p in plan:
+#         if p == 'L':
+#             now = (now[0], now[1] - 1)
+#             if now[1] < 1 :
+#                 now = (now[0],now[1]+1)
+#         elif p == 'R':
+#             now = (now[0], now[1] + 1) 
+#             if now[1] < 1 :
+#                 now = (now[0],now[1]-1)   
+#         elif p == 'U':
+#             now = (now[0] - 1,now[1])
+#             if now[0] < 1:
+#                 now = (now[0]+1,now[1])
+#         elif p == 'D':
+#             now = (now[0] + 1, now[1])
+#             if now[0] > N:
+#                 now = (now[0]-1, now[1])
+#     return now
+
+# --------------
+# p113 예제 4-2 <시각>
+# <아이디어>
+# 1) 정수 N = input
+# 2) time = ['00','00','00'] ~ ['0N','59','59'] 
+# => 첫번째 인덱스는 0~N범위
+# => 두번째, 세번째 인덱스는 0~59범위 
+# 3) 3이 들어있으면 count
+
+# <예제>
+N = 5
+count = 0 #시작점
+for i in range(0,N+1): # i = 첫번째 인덱스 범위값
+    for j in range(0,60): # j = 두번째 인덱스 범위값
+        for z in range(0,60): # z = 세번째 인덱스 범위값
+            if '3' in str(i) + str(j) +str(z): # time 리스트(모든 인덱스값) 에서 '3'이 포함된 경우 
+                count +=1 
+
+print(count)
+# <프로그램구현>
+def solution(N):
+    N = input("정수를 입력하세요: ")
+    count = 0
+    for i in range(0,N+1): 
+        for j in range(0,60):
+            for z in range(0,60): 
+                if '3' in str(i) + str(j) +str(z): 
+                    count +=1 
+    return count
+
+# --------------
+# p115 실전문제 <왕실의 나이트>
+# <아이디어>
+
+# <예제>
+
+# <프로그램 구현>
+
+
+# ---------------
+# p118 실전문제 <게임개발>
+
+j
+
+
